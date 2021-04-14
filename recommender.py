95c376f05d9e39fa9f6e141e58cc8dcc769087af
kuangzijian <kuangzijian1@hotmail.com>
2021-04-14 16:08:17 -0600

    modified recommender to read data from our database, and train/predict the model based on known info

--- recommender.py
+++ recommender.py
@@ -1,26 +1,42 @@
 import numpy as np
-from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
+from lightfm.data import Dataset
+import sqlite3
 
-# fetch movie data having a minimum rating of five
-data = fetch_movielens(min_rating=5.0)
+# Create a SQL connection to our SQLite database
+con = sqlite3.connect("db.sqlite3")
+cur = con.cursor()
+
+# The result of a "cursor.execute" can be iterated over by row
+data = []
+
+for row in cur.execute('SELECT userId, movieId, rating FROM RecoFramework_ratings;'):
+    data.append(row)
+
+dataset = Dataset()
+dataset.fit((i[0] for i in data), (m[1] for m in data))
+interactions, ratings = dataset.build_interactions(data)
+
+# Be sure to close the connection
+con.close()
+
 model = LightFM(loss='warp')
 
 # train lightFM model using fit method
-model.fit(data['train'], epochs=30, num_threads=2)
+model.fit(ratings, epochs=30, num_threads=2)
 
 def Recomender(model, data, userid):
     # number of movies and users in our dataset
-    n_users, n_items = data['train'].shape
+    n_users, n_items = ratings.shape
 
     # list of movie indices that user likes
-    known = data['item_labels'][data['train'].tocsr()[userid].indices]
+    known = ratings.tocsr()[userid].indices
 
     # predicting the scores
     scores = model.predict(userid, np.arange(n_items))
 
     # ranking them in non increasing order
-    top = data['item_labels'][np.argsort(-scores)]
+    top = np.argsort(-scores)
 
     # display results
     print("User %s" % userid)
@@ -32,4 +48,4 @@ def Recomender(model, data, userid):
         print("%s" % x)
 
 # predicting top 5 movies for the user 23
-Recomender(model, data, 23)
+Recomender(model, ratings, 23)
