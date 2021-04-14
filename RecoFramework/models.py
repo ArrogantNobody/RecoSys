c5a15334f41eb2db3d9c79eaca2e54ddad45eef4
txrxrxr <xtie@ualberta.ca>
2021-04-14 00:21:02 -0600

    Added movie and rating tables.

--- RecoFramework/models.py
+++ RecoFramework/models.py
@@ -8,3 +8,13 @@ class UserInfo(models.Model):
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=32)
     age = models.IntegerField()
+
+class Ratings(models.Model):
+    userId = models.IntegerField()
+    movieId = models.IntegerField()
+    rating = models.FloatField()
+
+class Movies(models.Model):
+    movieId = models.IntegerField(primary_key=True)
+    imdbId = models.IntegerField()
+    title = models.CharField(max_length=140)
