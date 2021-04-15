554558762a021126e285898d45ebf2060c8dc7f4
txrxrxr <xtie@ualberta.ca>
2021-04-14 21:16:09 -0600

    Added evaluation code.

--- recommender.py
+++ recommender.py
@@ -2,6 +2,9 @@ import numpy as np
 from lightfm import LightFM
 from lightfm.data import Dataset
 import sqlite3
+from lightfm.evaluation import precision_at_k
+from lightfm.evaluation import auc_score
+from lightfm.cross_validation import random_train_test_split
 
 def fetch_data():
     # Create a SQL connection to our SQLite database
@@ -18,7 +21,7 @@ def fetch_data():
     for row in cur.execute('SELECT movieId FROM RecoFramework_movies;'):
         movies.append(row[0])
 
-    for row in cur.execute('SELECT userId, movieId, rating FROM RecoFramework_ratings WHERE rating > 4;'):
+    for row in cur.execute('SELECT userId, movieId, rating FROM RecoFramework_ratings WHERE rating = 5;'):
         data.append(row)
 
     dataset = Dataset()
@@ -29,21 +32,23 @@ def fetch_data():
     # Be sure to close the connection
     con.close()
 
+    train, test = random_train_test_split(interactions)
+
     model = LightFM(loss='warp')
 
     # train lightFM model using fit method
     print("Starting training the model...")
-    model.fit(ratings, epochs=30, num_threads=2)
+    model.fit_partial(train, epochs=30, num_threads=2)
 
     user_dict = dataset._user_id_mapping
     movie_dict = dataset._item_id_mapping
 
-    return model, ratings, user_dict, movie_dict
+    return model, ratings, user_dict, movie_dict, train, test
 
 def recommend_by_userid(userid):
 
     # fetch data movie data and trained model from our database
-    model, ratings, user_dict, movie_dict = fetch_data()
+    model, _, user_dict, movie_dict, train, test = fetch_data()
     movie_indices = list(movie_dict.keys())
     movie_ids = list(movie_dict.values())
 
@@ -51,11 +56,11 @@ def recommend_by_userid(userid):
     recommended_movies = []
 
     # number of movies and users in our dataset
-    n_users, n_items = ratings.shape
+    n_users, n_items = train.shape
 
     user_index = user_dict[userid]
     # list of movie indices that user likes
-    known = ratings.tocsr()[user_index].indices
+    known = train.tocsr()[user_index].indices
     for i, movie in enumerate(known):
         known_movies.append(movie_indices[movie_ids.index(movie)])
     # predicting the scores
@@ -75,11 +80,21 @@ def recommend_by_userid(userid):
     for x in recommended_movies[:10]:
         print("%s" % x)
 
+    evaluation(model, train, test)
+
     return recommended_movies[:10]
 
-def evaluation():
-    print("Starting evaluation our model...")
+def evaluation(model, train, test):
+    print("\nStarting evaluation our model...")
+    train_precision = precision_at_k(model, train, k=10).mean()
+    test_precision = precision_at_k(model, test, k=10).mean()
+
+    train_auc = auc_score(model, train).mean()
+    test_auc = auc_score(model, test).mean()
+
+    print('Precision: train %.2f, test %.2f.' % (train_precision, test_precision))
+    print('AUC: train %.2f, test %.2f.' % (train_auc, test_auc))
 
 if __name__ == '__main__':
     # predicting top 10 movies for the user 23
-    recommended_movies = recommend_by_userid(23)
+    recommended_movies = recommend_by_userid(4)
